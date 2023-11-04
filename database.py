3861e6d797f09e0f80449e8398ae946967c28ab3
Kacper Masny <kacper.masny2000@gmail.com>
2023-11-04 16:33:07 +0100

    first commit

---
+++ database.py
@@ -0,0 +1,16 @@
+from dotenv import load_env
+import os
+from  sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+load_env()
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+URL_DATABASE = f'mysql+pymysql://root:{DB_PASSWORD}@localhost:3306/{DB_NAME}'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
